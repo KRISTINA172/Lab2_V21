c0e7f350b7a8031729c343d836e0883a1a8ae3a2
Kristina <rusnakhristina666@gmail.com>
2025-11-20 14:13:45 +0200

    Add files via upload

---
+++ Task3_Tests.cs
@@ -0,0 +1,23 @@
+﻿using Lab2_V21_3;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace Lab2_V21_3.Tests
+{
+    [TestClass]
+    public class ProgramTests
+    {
+        [TestMethod]
+        public void Test_CountLessThanSeven() // Тест для методу CountLessThanSeven
+        {
+            
+            int[] arr = { 2, 10,1 };   // Вхідні дані
+
+            int expected = 2; // Очікуваний результат
+
+            int actual = Program.CountLessThanSeven(arr); // Виклик методу 
+
+            Assert.AreEqual(expected, actual, //Перевірка
+                "Кількість елементів < 7 обчислена неправильно.");
+        }
+    }
+}
